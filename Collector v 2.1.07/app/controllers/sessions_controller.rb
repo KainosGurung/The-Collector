5239a69ea9c919683aa4882e8a82c3255da5801d
KainosGurung <KainosGurung@gmail.com>
2015-03-03 17:01:15 -0700

    2.1.-7

---
+++ Collector v 2.1.07/app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+
+  def new
+    if session[:self_provider_id].present?
+      redirect_to root_path
+    end
+    @provider = Provider.new
+  end
+
+  def create
+    @provider = Provider.authenticate(params[:provider_name], params[:password])
+
+    if @provider
+      session[:self_provider_id] = @provider.id
+      session[:self_provider_name] = @provider.provider_name
+      session[:self_admin] = @provider.admin
+      if @provider.admin
+        redirect_to admin_home_path
+      else
+        redirect_to pv_patient_record_arrays_path
+      end
+    else
+      flash.now.alert = 'Log in failed'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:self_provider_id] = nil
+    session[:self_provider_name] = nil
+    session[:self_admin] = nil
+    redirect_to root_path, :notice => 'Logged out!'
+  end
+end
