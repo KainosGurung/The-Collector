787b2a31cfc90f3e9a38205d028896834fd65de3
jake bourgoin <jbour72@gmail.com>
2015-03-09 12:40:29 -0600

    added help messages

---
+++ Collector v 2.1.10/app/controllers/help_controller.rb
@@ -0,0 +1,17 @@
+class HelpController < ActionController::Base
+
+  def pv_behavior_record_arrays_help
+  end
+
+  def admin_home_help
+  end
+
+  def providers_index_help
+  end
+
+  def patients_index_help
+  end
+
+  def behaviors_index_help
+  end
+end
