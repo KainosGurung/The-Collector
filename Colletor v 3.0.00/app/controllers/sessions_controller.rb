3bb3f9c3939532b2569a1f996ab1829838739a0c
KainosGurung <KainosGurung@gmail.com>
2015-04-11 15:37:04 -0600

    finished adding super_admin feature. seperated new/edit/activate/deactivate/destroy commands into provider/admin

---
+++ Colletor v 3.0.00/app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+
+  def new
+    # if session[:self_provider_id].present?
+    #   redirect_to root_path
+    # end
+    @provider = Provider.new
+  end
+
+  def create
+    @provider = Provider.authenticate(params[:provider_name], params[:password])
+
+    if @provider
+      session[:self_provider_id] = @provider.id
+      session[:self_provider_name] = @provider.provider_name
+      session[:self_admin] = @provider.admin
+      if @provider.admin == 0
+        redirect_to pv_patient_record_arrays_path
+      elsif @provider.admin == 1
+        redirect_to admin_home_path
+      elsif @provider.admin == 2
+        session[:self_super_admin] = 1
+        redirect_to super_admin_home_path
+      end
+    else
+      flash[:error] = 'Log in failed'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:self_provider_id] = nil
+    session[:self_provider_name] = nil
+    session[:self_admin] = nil
+    flash[:info] = 'Logged out!'
+    redirect_to root_path
+  end
+end
---
+++ Colletor v 3.0.00/app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+class WelcomeController < ApplicationController
+  before_filter :authenticate_super_admin_login, only: :super_admin_home
+  before_filter :authenticate_admin_login, only: :admin_home
+  before_filter :authenticate_provider_login, only: :provider_home
+
+  def super_admin_home
+  end
+
+  def admin_home
+  end
+
+  def provider_home
+  end
+
+end
